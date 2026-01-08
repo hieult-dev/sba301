5b728e951351ab06d38c8ff7f38895b544462302
Lê Trung Hiếu <lehieuhn153@gmail.com>
2026-01-08 23:18:46 +0700

    .

--- connect-supabase/src/main/resources/db/migration/V2__insert_data.sql
+++
@@ -1,5 +0,0 @@
-insert into users_test (id, username, email, created_at)
-values
-    (1, 'admin', 'admin@example.com', now()),
-    (2, 'test_user', 'test@example.com', now()),
-    (3, 'demo_user', 'demo@example.com', now());
--- connect-supabase/src/main/resources/db/migration/V3__init_tabel.sql
+++
@@ -1,7 +0,0 @@
-create table users_test_2
-(
-    id         bigserial primary key,
-    username   varchar(100) not null,
-    email      varchar(255),
-    created_at timestamptz default now()
-);
